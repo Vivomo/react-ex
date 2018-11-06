3933b45acff5f16a43076c500d7079edc4743d05
Vimo <929992114@qq.com>
2018-11-06 20:06:20 +0800

    test scss

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,5 +1,13 @@
 import React from "react";
+import './about.scss'
 
-const About = () => <div>About</div>;
+const About = () => {
+    return (
+        <>
+            <div>About</div>
+            <p class="red">red</p>
+        </>
+    )
+};
 
 export default About;
