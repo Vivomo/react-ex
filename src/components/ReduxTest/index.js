66f7c1bdb138782ffa4a7e90dcfb3e11bde5ba26
Vimo <929992114@qq.com>
2019-01-11 01:27:23 +0800

    no message

---
+++ src/components/ReduxTest/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Provider } from 'react-redux';
+
+const ReduxTest = () => {
+    return (
+        <>
+            <div>ReduxTest</div>
+        </>
+    )
+};
+
+export default ReduxTest;
