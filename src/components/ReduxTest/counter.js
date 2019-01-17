4133c5ad73c76c7d36d1b9d7339a92050ba9297f
Vivomo <929992114@qq.com>
2019-01-17 17:48:44 +0800

    store 直接 更改state

--- src/components/ReduxTest/counter.js
+++ src/components/ReduxTest/counter.js
@@ -4,14 +4,24 @@ import PropTypes from 'prop-types';
 
 import {setCount} from '../../store/action';
 import {bindActionCreators} from 'redux';
+import store from '../../store';
 
 class Counter extends Component {
+
     render() {
         console.log(this.context);
         return (
             <div>
                 <div>{this.props.count}</div>
                 <button onClick={() => this.props.setCount(this.props.count + 1)}>count++</button>
+                <button onClick={() => {
+                    store.dispatch({
+                        type: 'SET_GLOBAL_COUNT',
+                        payload: {
+                            value: 10
+                        }
+                    });
+                }}>store dispatch</button>
             </div>
         );
     }
