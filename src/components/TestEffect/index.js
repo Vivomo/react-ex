321d8172ee82b892ee829d90865b0f3c5cc3b56d
Vivomo <929992114@qq.com>
2019-06-12 16:37:21 +0800

    test effect 2

--- src/components/TestEffect/index.js
+++ src/components/TestEffect/index.js
@@ -2,6 +2,7 @@ import React, {useState} from 'react';
 
 const TestEffect = () => {
     let [num, setNum] = useState(0);
+    let [obj, setObj] = useState({num: 0});
 
     let delayLog = () => {
         setTimeout(() => {
@@ -9,11 +10,26 @@ const TestEffect = () => {
         }, 3000);
     };
 
+    let delayLog2 = () => {
+        setTimeout(() => {
+            console.log(JSON.stringify(obj), obj);
+        }, 3000);
+    };
+
     return (
         <div>
             <span>num: {num}</span>
-            <button onClick={() => setNum(num + 1)}>add</button>
+            <button onClick={() => setNum(num + 1)}>add num</button>
             <button onClick={delayLog}>log</button>
+
+            <br/>
+            <span>obj.num: {obj.num}</span>
+
+            <button onClick={() => {
+                obj.num++;
+                setObj({...obj});
+            }}>add obj.num</button>
+            <button onClick={delayLog2}>log obj</button>
         </div>
     );
 };
