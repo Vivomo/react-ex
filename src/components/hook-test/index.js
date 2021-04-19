b19ca2cff431ba85c57ba4558bd7d3bca7c40ca1
vivomo <929992114@qq.com>
2021-04-20 04:05:47 +0800

    自定义hooks测试

--- src/components/hook-test/index.js
+++ src/components/hook-test/index.js
@@ -1,18 +1,54 @@
-import React from 'react';
-import {useState} from 'react';
+import React, {useState, useEffect, useRef} from 'react';
 
+const formatList = (list) => {
+
+    const intervalRef = useRef(0);
+    const [data, setData] = useState({
+        data: []
+    });
+
+    useEffect(() => {
+        if (list.length === 0) {
+            return;
+        }
+        const cacheData = list.map(item => ({
+            num: item,
+            doubleNum: 0
+        }));
+
+        let cursor = 0;
+        intervalRef.current = setInterval(() => {
+            cacheData[cursor].doubleNum = cacheData[cursor].num * 2;
+            setData({
+                data: cacheData
+            });
+            cursor++;
+            if (cursor === cacheData.length) {
+                clearInterval(intervalRef.current);
+            }
+        }, 1000);
+    }, [list]);
+    return data;
+};
 
 const HooksTest = () => {
-    let [num, setNum] = useState(0);
 
-    console.log(setNum);
+    const [list, setList] = useState([]);
+
+    const {data} = formatList(list);
+
+    useEffect(() => {
+        setList(Array(5).fill(0).map((_, index) => index));
+    }, []);
+
+    console.log(data);
+
     return (
-        <div>
-            <button onClick={() => {
-                setNum(num + 1);
-            }}>Number ++</button>
-            {num}
-        </div>
+        <ul>
+            {
+                data.map(item => <li key={item.num}>{item.num}: {item.doubleNum}</li>)
+            }
+        </ul>
     );
 };
 
